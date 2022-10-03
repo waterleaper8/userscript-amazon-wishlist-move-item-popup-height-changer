3a37487b384643320d5400453c350b52306df186
waterleaper <waterleaper8@gmail.com>
2022-10-03 13:01:00 +0900

    ライセンス追記

--- main.js
+++ main.js
@@ -7,6 +7,7 @@
 // @match        https://www.amazon.co.jp/hz/wishlist/ls/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
+// @license MIT
 // ==/UserScript==
 
 ;(function () {
